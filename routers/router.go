39b1e7fbb4f5fb23784d4c3c6ef89d53f05b484e
farhan <farhanasfar21@gmail.com>
2026-03-04 16:08:16 +0600

    Add middleware check

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"properties-by-location/controllers"
+	"properties-by-location/middlewares"
 
 	"github.com/beego/beego/v2/server/web"
 )
@@ -9,6 +10,7 @@ import (
 func init() {
 
 	ns := web.NewNamespace("/api/v1",
+		web.NSBefore(middlewares.ValidateAPIKey),
 		web.NSRouter("/get-properties/:location", &controllers.PropertyController{}, "get:Get"),
 	)
 	web.AddNamespace(ns)
