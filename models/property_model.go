bbabc9479005e39c4f4b01a23311d3e75dd3c63f
farhan <farhanasfar21@gmail.com>
2026-03-04 10:48:45 +0600

    Update property_model
    Add ID and Slug field

--- models/property_model.go
+++ models/property_model.go
@@ -1,8 +1,10 @@
 package models
 
 type Property struct {
+	ID           string `json:"id"`
 	Name         string `json:"Name"`
 	PropertyName string `json:"property_name"`
 	Country      string `json:"Country"`
 	City         string `json:"City"`
+	Slug         string `json:"slug"`
 }
