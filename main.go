f12e8615e78483496fee430e62cf7597fa558b60
arnob <farhanasfar21@gmail.com>
2026-03-04 22:51:53 +0600

    Validate config keys at startup

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
 	_ "properties-by-location/routers"
-	beego "github.com/beego/beego/v2/server/web"
+
+	"github.com/beego/beego/v2/server/web"
 )
 
 func main() {
-	beego.Run()
+	// validating config keys
+	mustConfig("travel_api_base_url")
+	mustConfig("travel_api_origin")
+	mustConfig("x_api_key")
+
+	web.Run()
 }
 
+func mustConfig(key string) {
+	val, err := web.AppConfig.String(key)
+
+	if err != nil || val == "" {
+		panic(fmt.Sprintf("Missing required config key: %s. Please add it into your app.conf file", key))
+	}
+}
