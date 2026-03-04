25794d6f6a794016724307c81025820b2d8d830f
farhan <farhanasfar21@gmail.com>
2026-03-04 13:13:23 +0600

    Add more fields to property model

--- models/property_model.go
+++ models/property_model.go
@@ -1,10 +1,14 @@
 package models
 
 type Property struct {
-	ID           string `json:"id"`
-	Name         string `json:"Name"`
-	PropertyName string `json:"property_name"`
-	Country      string `json:"Country"`
-	City         string `json:"City"`
-	Slug         string `json:"slug"`
+	ID           string  `json:"id"`
+	PropertyName string  `json:"property_name"`
+	PropertyType string  `json:"property_type"`
+	Country      string  `json:"country"`
+	City         string  `json:"city"`
+	Slug         string  `json:"slug"`
+	URL          string  `json:"url"`
+	StarRating   int     `json:"star_rating"`
+	ReviewScore  float64 `json:"review_score"`
+	Price        float64 `json:"price"`
 }
