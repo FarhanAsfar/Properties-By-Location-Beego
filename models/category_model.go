aca310345986adcaceee6465acc6605b61e21f5a
farhan <farhanasfar21@gmail.com>
2026-03-04 12:45:15 +0600

    Update category model: fix model structure

--- models/category_model.go
+++ models/category_model.go
@@ -2,20 +2,35 @@ package models
 
 // response from TravelAPI category endpoint
 type CategoryResponse struct {
-	Data CategoryData `json:"data"`
+	Result CategoryResult `json:"Result"`
 }
 
-type CategoryData struct {
-	Properties []PropertyItem `json:"properties"`
+type CategoryResult struct {
+	Count int            `json:"Count"`
+	Items []CategoryItem `json:"Items"`
 }
 
-// each property returned by the api
+type CategoryItem struct {
+	ID       string       `json:"ID"`
+	GeoInfo  ItemGeoInfo  `json:"GeoInfo"`
+	Property ItemProperty `json:"Property"`
+}
+type ItemGeoInfo struct {
+	City    string `json:"City"`
+	Country string `json:"Country"`
+	Display string `json:"Display"`
+}
 
-type PropertyItem struct {
-	ID           string `json:"id"`
-	Name         string `json:"Name"`
-	PropertyName string `json:"property_name"`
-	Country      string `json:"Country"`
-	City         string `json:"City"`
-	Slug         string `json:"Slug"`
+type ItemProperty struct {
+	PropertyName string  `json:"PropertyName"`
+	PropertySlug string  `json:"PropertySlug"`
+	PropertyType string  `json:"PropertyType"`
+	Price        float64 `json:"Price"`
+	ReviewScore  float64 `json:"ReviewScore"`
+	StarRating   int     `json:"StarRating"`
+	FeatureImage string  `json:"FeatureImage"`
+}
+
+type ItemPartner struct {
+	URL string `json:"URL"`
 }
