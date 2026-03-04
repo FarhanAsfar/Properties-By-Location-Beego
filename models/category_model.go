b77723640c1c20a6ef7f7d3014b2d4a5e6103cd1
arnob <farhanasfar21@gmail.com>
2026-03-04 06:11:47 +0600

    Create category-model: store response from category endpoint

---
+++ models/category_model.go
@@ -0,0 +1,20 @@
+package models
+
+// response from TravelAPI category endpoint
+type CategoryResponse struct {
+	Data CategoryData `json:"data"`
+}
+
+type CategoryData struct {
+	Properties []PropertyItem `json:"properties"`
+}
+
+// each property returned by the api
+
+type PropertyItem struct {
+	ID      string `json:"id"`
+	Name    string `json:"Name"`
+	Country string `json:"Country"`
+	City    string `json:"City"`
+	Slug    string `json:"Slug"`
+}
