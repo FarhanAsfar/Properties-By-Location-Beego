8d962fe78321267e571f291f290fb9399b59e8e0
farhan <farhanasfar21@gmail.com>
2026-03-04 10:42:00 +0600

    Implement fetchProperties function

--- services/property_service.go
+++ services/property_service.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"fmt"
 	"properties-by-location/models"
+	"properties-by-location/utils"
+	"strings"
 	"time"
+
+	"github.com/beego/beego/v2/server/web"
 )
 
 type propertyResult struct {
@@ -33,4 +37,49 @@ func GetPropertiesByLocation(location string) ([]models.Property, error) {
 	case <-ctx.Done():
 		return nil, fmt.Errorf("Request timed out while fetching properties")
 	}
+}
+
+// perform two external api calls
+func fetchProperties(ctx context.Context, location string) ([]models.Property, error) {
+	baseURL, _ := web.AppConfig.String("travel_api_base_url")
+	origin, _ := web.AppConfig.String("travel_api_origin")
+
+	// resolve location slug
+	locationURL := fmt.Sprintf("%s/v1/location?keyword=%s", baseURL, location)
+
+	locationResp, err := utils.FetchandDecode[models.LocationResponse](ctx, locationURL, nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch location: %w", err)
+	}
+
+	if locationResp == nil || len(locationResp.Data) == 0 {
+		return nil, fmt.Errorf("No location data found for: %s", location)
+	}
+
+	// extract slug from the result
+	rawSlug := locationResp.Data[0].Slug
+
+	//replace '/' with ':'
+	categorySlug := strings.ReplaceAll(rawSlug, "/", ":")
+
+	categoryURL := fmt.Sprintf("%s/v1/category/details/%s?items=1", baseURL, categorySlug)
+
+	headers := map[string]string{
+		"Origin": origin,
+	}
+
+	categoryResp, err := utils.FetchandDecode[models.CategoryResponse](ctx, categoryURL, headers)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch properties: %w", err)
+	}
+
+	if categoryResp == nil {
+		return nil, fmt.Errorf("Empty response from category API")
+	}
+
+	properties := flattenProperties(categoryResp.Data.Properties)
+
+	return properties, nil
 }
